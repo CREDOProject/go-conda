def821578b3be1518cbcfb991e0654d89ac62db7
Eliseo Martelli <me@eliseomartelli.it>
2024-03-01 12:14:04 +0100

    feat: initial commit

---
+++ conda.go
@@ -0,0 +1,49 @@
+package goconda
+
+type verb string
+
+const (
+	Install verb = "install"
+)
+
+type conda struct {
+	verb        verb
+	packageInfo PackageInfo
+	dryRun      bool
+}
+
+type command struct {
+	binaryName      *string
+	binaryArguments []string
+}
+
+type PackageInfo struct {
+	PackageName string
+	Channel     string
+}
+
+// Installs a package.
+// https://docs.anaconda.com/free/working-with-conda/packages/install-packages/
+func (c *conda) Install(info *PackageInfo) *conda {
+	c.packageInfo = *info
+	c.verb = Install
+	return c
+}
+
+// Enables dry-run.
+// https://docs.conda.io/projects/conda/en/latest/commands/install.html#output,-prompt,-and-flow-control-options
+func (c *conda) DryRun() *conda {
+	c.dryRun = true
+	return c
+}
+
+// Start a new Conda command.
+// https://docs.anaconda.com/free/working-with-conda/
+func New() *conda {
+	return &conda{}
+}
+
+// Build the Conda command so it can be run.
+func (p *conda) Seal() (*command, error) {
+	return nil, nil
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/CREDOProject/go-conda
+
+go 1.22.0
